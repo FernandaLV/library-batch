bfb563831c88198d4feb46e1ecbdba65eb8c04ca
Logon Aluno <logonaluno@local.com>
2020-02-12 21:13:48 -0300

    delete file

--- src/main/java/br/com/fiap/librarybatch/LibraryBatchApplication.java
+++ src/main/java/br/com/fiap/librarybatch/LibraryBatchApplication.java
@@ -7,6 +7,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableBatchProcessing
 public class LibraryBatchApplication {
 
+	Logger logger = LoggerFactory.getLogget(LibraryBatchApplication.class.getName());
+
+	@Bean
+	public Tasklet deleteFile(@Value("${file.path}") String path) {
+		return (contribution, chunkContext)->{
+			File file = Paths.get(path).toFile;
+			if (file.delete()) {
+				logger.info("Arquivo deletado");
+			}
+			return RepeatStatus.FINISHED;
+		};
+	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Step deleteStep(StepBuilderFactory stepBuilderFactory,
+						   Tasklet tasklet) {
+		return stepBuilderFactory.get("delete file step")
+				.tasklet(tasklet)
+				.build();
+	}
+
+	@Bean
+	public Job deleteJob(JobBuilderFactory jobBuilderFactory,
+						 Step step) {
+		return jobBuilderFactory.get("delete job")
+				.start(step)
+				.build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryBatchApplication.class, args);
 	}
